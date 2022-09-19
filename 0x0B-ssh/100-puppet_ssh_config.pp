9a8a416bc3699e1a6365a2a46fbfed16127a18ec
Efa-Iwa <gistend@gmail.com>
2022-09-18 18:00:56 -0700

    Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+# Changes to our SSH config file
+
+exec { 'echo':
+  path    => 'usr/bin:/bin',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  returns => [0,1],
+}
