2831bcec58095ce9356873e1f0b8b2dd6a137cfc
root <gistend@gmail.com>
2022-11-03 02:06:43 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# Fixes bad `phpp` extensions to `php`
+# use strace to find out why apache is returning a 500 error
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'fix_500_erri':
+  command => "/bin/sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
 }
